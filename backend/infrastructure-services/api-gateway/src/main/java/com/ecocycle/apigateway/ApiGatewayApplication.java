17a300688b42c5e170593b24566a1ce03e286136
Khanh <voduykhanh.6275@gmail.com>
2026-04-01 12:40:13 +0700

    update code

--- backend/infrastructure-services/api-gateway/src/main/java/com/ecocycle/apigateway/ApiGatewayApplication.java
+++ backend/infrastructure-services/api-gateway/src/main/java/com/ecocycle/apigateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.ecocycle.gateway;
+﻿package com.ecocycle.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
