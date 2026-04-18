e583b4093cc24b61d2630a25d3b717ba1a7caddf
ntp2203 <nguyentienphatpro@gmail.com>
2026-04-18 22:21:35 +0700

    infra: add docker-compose services and db init for analytics and enterprise

--- infrastructure/docker/init-db.sql
+++ infrastructure/docker/init-db.sql
@@ -1,14 +1,16 @@
--- Script tự động tạo tất cả databases cho từng Microservice khi PostgreSQL khởi động
--- Chạy tự động nhờ volume mount vào /docker-entrypoint-initdb.d/
 
 CREATE DATABASE auth_db;
 CREATE DATABASE user_db;
 CREATE DATABASE collection_db;
 CREATE DATABASE reward_db;
 CREATE DATABASE notification_db;
+CREATE DATABASE ecocycle_analytics;
+CREATE DATABASE ecocycle_enterprise;
 
 GRANT ALL PRIVILEGES ON DATABASE auth_db TO ecocycle;
 GRANT ALL PRIVILEGES ON DATABASE user_db TO ecocycle;
 GRANT ALL PRIVILEGES ON DATABASE collection_db TO ecocycle;
 GRANT ALL PRIVILEGES ON DATABASE reward_db TO ecocycle;
 GRANT ALL PRIVILEGES ON DATABASE notification_db TO ecocycle;
+GRANT ALL PRIVILEGES ON DATABASE ecocycle_analytics TO ecocycle;
+GRANT ALL PRIVILEGES ON DATABASE ecocycle_enterprise TO ecocycle;
